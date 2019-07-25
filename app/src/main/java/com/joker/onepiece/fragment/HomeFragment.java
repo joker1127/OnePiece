cd66f5fd1be52a442c25e1e91fe1467d7712d1cf
Joker <liming@huomaotv.com>
2019-07-25 16:50:09 +0800

    添加LiveBus,LiveData

--- app/src/main/java/com/joker/onepiece/fragment/HomeFragment.java
+++ app/src/main/java/com/joker/onepiece/fragment/HomeFragment.java
@@ -2,25 +2,13 @@ package com.joker.onepiece.fragment;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
-import com.joker.libcommon.LatestMovie;
-import com.joker.libcommon.jetpack.LifecycleFragment;
-import com.joker.libcommon.retrofit.APIService;
-import com.joker.libcommon.retrofit.RetrofitManager;
+import com.joker.mvvm.LatestMovie;
+import com.joker.mvvm.jetpack.LifecycleFragment;
 import com.joker.onepiece.R;
 import com.joker.onepiece.viewmodel.HomeViewModel;
 
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-import io.reactivex.subscribers.DisposableSubscriber;
 
 public class HomeFragment extends LifecycleFragment<HomeViewModel> {
 
@@ -33,12 +21,22 @@ public class HomeFragment extends LifecycleFragment<HomeViewModel> {
     }
 
     @Override
+    public void dataObserver() {
+
+        registerObserver("Home", LatestMovie.class).observe(this, value -> {
+            Log.e("SAX", "value: " + value.getTitle());
+
+        });
+
+    }
+
+    @Override
     public int getLayoutResId() {
         return R.layout.fragment_home;
     }
 
 
-    private void getData(){
+    private void getData() {
         viewModel.getHomeData();
     }
 
